eadf3f1409d390ffe66a24e098c4d3fb3ef14651
chandraiwayan <chandraiwayan.ic@gmail.com>
2022-01-18 13:28:44 +0700

    web angkatan

---
+++ Kumpulan Artikel/Query.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $('#hide1').click(function(){
+        $('.puisi1').hide(10000);
+    });
+    $('#show1').click(function(){
+        $('.puisi1').show(10000);
+    });
+
+    $('#hide2').click(function(){
+        $('.puisi2').hide(10000);
+    });
+    $('#show2').click(function(){
+        $('.puisi2').show(10000);
+    });
+
+    $('#hide3').click(function(){
+        $('.puisi3').hide(10000);
+    });
+    $('#show3').click(function(){
+        $('.puisi3').show(10000);
+    });
+});
---
+++ Titiktemu/js/filter.js
@@ -0,0 +1,19 @@
+function filter() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('filterForm');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("box");
+  li = ul.getElementsByClassName("innerbox");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("div")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
